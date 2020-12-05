5698f4c4fff0a0d1ec0c9bbee9f67d65b0fcef31
zessx <contact@zes.sx>
2020-12-05 22:41:50 +0100

    :lipstick: Recipes

--- frontend/src/components/Recipes.vue
+++ frontend/src/components/Recipes.vue
@@ -2,18 +2,78 @@
   <div class="container">
     <h1>{{ $t('navbar.recipes') }}</h1>
 
+    <RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe" />
+
     <Navbar page="recipes" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Recipes'
+  name: 'Recipes',
+  data: function () {
+    return {
+      recipes: [
+        {
+          id: 1,
+          title: 'Porc tonkatsu',
+          image: 'https://www.placecage.com/200/200',
+          duration: 15,
+          portions: 2
+        },
+        {
+          id: 2,
+          title: 'Bouillon de volaille',
+          image: 'https://www.placecage.com/201/201',
+          duration: 90,
+          portions: null
+        },
+        {
+          id: 3,
+          title: 'Ramen de poulet au petits oignons marinés',
+          image: 'https://www.placecage.com/202/202',
+          duration: 60,
+          portions: 4
+        },
+        {
+          id: 4,
+          title: 'Crêpes',
+          image: 'https://www.placecage.com/203/203',
+          duration: null,
+          portions: 12
+        },
+        {
+          id: 5,
+          title: 'Pancakes',
+          image: 'https://www.placecage.com/204/204',
+          duration: 15,
+          portions: 2
+        },
+        {
+          id: 6,
+          title: 'Tartiflette',
+          image: 'https://www.placecage.com/205/205',
+          duration: 75,
+          portions: 4
+        },
+        {
+          id: 7,
+          title: 'Wraps',
+          image: 'https://www.placecage.com/206/206',
+          duration: 60,
+          portions: 4
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
 h1 {
   text-align: center;
+}
+.recipe {
+  margin-bottom: 0.5em;
 }
 </style>
---
+++ frontend/src/components/layout/RecipeCard.vue
@@ -0,0 +1,92 @@
+<template>
+  <article class="recipe">
+    <img :src="recipe.image" :alt="recipe.title">
+    <div>
+      <h3>{{ recipe.title }}</h3>
+      <footer>
+        <div class="duration" v-if="recipe.duration">
+          <BaseIcon name="timing" />
+          <span>{{ recipe.duration }}</span>
+        </div>
+        <div class="portions" v-if="recipe.portions">
+          <BaseIcon name="portion" />
+          <span>{{ recipe.portions }}</span>
+        </div>
+        <BaseButton size="small" icon="add" />
+      </footer>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'RecipeCard',
+  props: {
+    recipe: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.recipe {
+  display: flex;
+  height: 6.25em;
+  padding: var(--spacing-01);
+  border-radius: 5px;
+  box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+  background-color: var(--white);
+
+  img {
+    display: block;
+    height: 100%;
+    margin-right: var(--spacing-03);
+    border-radius: 3px;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    padding: var(--spacing-02);
+  }
+
+  h3 {
+    margin: 0;
+    font-size: 1.125em;
+  }
+
+  footer {
+    display: flex;
+    align-items: flex-end;
+    margin-top: auto;
+
+    .duration {
+
+      + .portions {
+        margin-left: var(--spacing-04);
+      }
+    }
+
+    .duration,
+    .portions {
+      i {
+        float: left;
+        height: 1.25em;
+        margin-right: var(--spacing-02);
+        color: var(--background-40);
+      }
+
+      span {
+        line-height: 1.35em;
+      }
+    }
+
+    button {
+      margin-left: auto;
+    }
+  }
+}
+</style>
